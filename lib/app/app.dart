d8c2903da4e077e84e5e1abc8689b5856e613e58
Tohir Mirxomitov <mirxtohir@gmail.com>
2024-07-08 19:02:17 +0500

    test

---
+++ lib/app/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class UzumBankApp extends StatelessWidget {
+  const UzumBankApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Uzum Bank',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Placeholder(),
+    );
+  }
+}
