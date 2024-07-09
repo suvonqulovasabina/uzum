05550c1cd6813aca29c068d706bc8ff493d653b3
Tohir Mirxomitov <mirxtohir@gmail.com>
2024-07-09 14:09:12 +0500

    language arb added

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -7,7 +7,6 @@ import 'package:uzum/l10n/l10n.dart';
 class UzumBankApp extends StatelessWidget {
   const UzumBankApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
