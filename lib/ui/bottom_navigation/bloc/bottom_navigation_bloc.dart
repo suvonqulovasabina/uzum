5f3f8da1e9c5bba55f87d5773d1be3fc8c25a225
Tohir Mirxomitov <mirxtohir@gmail.com>
2024-07-11 13:09:16 +0500

    language

---
+++ lib/ui/bottom_navigation/bloc/bottom_navigation_bloc.dart
@@ -0,0 +1,14 @@
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:freezed_annotation/freezed_annotation.dart';
+//
+// part 'bottom_navigation_event.dart';
+// part 'bottom_navigation_state.dart';
+// part 'bottom_navigation_bloc.freezed.dart';
+//
+// class BottomNavigationBloc extends Bloc<BottomNavigationEvent, BottomNavigationState> {
+//   BottomNavigationBloc() : super(const BottomNavigationState.initial()) {
+//     on<BottomNavigationEvent>((event, emit) {
+//       // TODO: implement event handler
+//     });
+//   }
+// }
---
+++ lib/ui/bottom_navigation/bloc/bottom_navigation_state.dart
@@ -0,0 +1,6 @@
+// part of 'bottom_navigation_bloc.dart';
+//
+// @freezed
+// class BottomNavigationState with _$BottomNavigationState {
+//   const factory BottomNavigationState.initial() = _Initial;
+// }
